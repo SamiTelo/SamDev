1a8abb7f0107199849d605efaf9305b4755209d3
Samuel Tiemtore <samueltiemtore10@gmail.com>
2026-05-15 17:15:10 +0000

    fix/contact-animation (#36)
    Co-authored-by: Sami Telo <samitelo@gmail.com>

--- src/features/contact/components/contact.tsx
+++ src/features/contact/components/contact.tsx
@@ -34,147 +34,138 @@ export const ContactSection = () => {
 
       {/*-------------------------Main Content Grid-------------------------------*/}
       <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-12 gap-18 sm:gap-20 md:gap-12 items-start mt-32">
-
         {/*---------------------------LEFT SIDE-------------------------------------*/}
-        {/* wrapper motion */}
-        <div className="overflow-hidden">
-          <motion.div
-            initial={{ x: -50, opacity: 0, scale: 0.95 }}
-            whileInView={{ x: 0, opacity: 1, scale: 1 }}
-            viewport={{ once: false, amount: 0.5 }}
-            transition={{
-              duration: 1.5,
-              type: "spring",
-              stiffness: 100,
-              damping: 20,
-            }}
-            className="lg:col-span-4 space-y-6"
-          >
-            <div className="flex items-center gap-5">
-              <div className="bg-orange-100 p-7 rounded-sm">
-                <MapPin className="text-orange-600 w-6 h-6" />
-              </div>
-              <div>
-                <h4 className="text-xl font-bold text-gray-800 mb-1">
-                  Address
-                </h4>
-                <p className="text-gray-500">Abidjan, Côte d’ivoire</p>
-              </div>
+        <motion.div
+          initial={{ x: -50, opacity: 0, scale: 0.95 }}
+          whileInView={{ x: 0, opacity: 1, scale: 1 }}
+          viewport={{ once: false, amount: 0.5 }}
+          transition={{
+            duration: 1.5,
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+          }}
+          className="lg:col-span-4 space-y-6"
+        >
+          <div className="flex items-center gap-5">
+            <div className="bg-orange-100 p-7 rounded-sm">
+              <MapPin className="text-orange-600 w-6 h-6" />
             </div>
+            <div>
+              <h4 className="text-xl font-bold text-gray-800 mb-1">Address</h4>
+              <p className="text-gray-500">Abidjan, Côte d’ivoire</p>
+            </div>
+          </div>
 
-            <div className="flex items-center gap-5">
-              <div className="bg-green-100 p-7 rounded-sm">
-                <Mail className="text-green-600 w-6 h-6" />
-              </div>
-              <div>
-                <h4 className="text-xl font-bold text-gray-800 mb-1">Email</h4>
-                <p className="text-gray-500">samueltiemtore10@gmail.com</p>
-              </div>
+          <div className="flex items-center gap-5">
+            <div className="bg-green-100 p-7 rounded-sm">
+              <Mail className="text-green-600 w-6 h-6" />
             </div>
+            <div>
+              <h4 className="text-xl font-bold text-gray-800 mb-1">Email</h4>
+              <p className="text-gray-500">samueltiemtore10@gmail.com</p>
+            </div>
+          </div>
 
-            <div className="flex items-center gap-5">
-              <div className="bg-purple-100 p-7 rounded-sm">
-                <Phone className="text-purple-600 w-6 h-6" />
-              </div>
-              <div>
-                <h4 className="text-xl font-bold text-gray-800 mb-1">Mobile</h4>
-                <p className="text-gray-500">+225 05 96 08 56 43</p>
-              </div>
+          <div className="flex items-center gap-5">
+            <div className="bg-purple-100 p-7 rounded-sm">
+              <Phone className="text-purple-600 w-6 h-6" />
             </div>
-          </motion.div>
-        </div>
+            <div>
+              <h4 className="text-xl font-bold text-gray-800 mb-1">Mobile</h4>
+              <p className="text-gray-500">+225 05 96 08 56 43</p>
+            </div>
+          </div>
+        </motion.div>
 
         {/*----------------------------RIGHT SIDE - FORM--------------------------------*/}
-        {/* wrapper motion */}
-        <div className="overflow-hidden">
-          <motion.div
-            initial={{ y: 50, opacity: 0, scale: 0.95 }}
-            whileInView={{ y: 0, opacity: 1, scale: 1 }}
-            viewport={{ once: false, amount: 0.5 }}
-            transition={{
-              duration: 1.5,
-              type: "spring",
-              stiffness: 100,
-              damping: 20,
-            }}
-            className="lg:col-span-8 px-0 md:pl-16 pb-8"
-          >
-            <form onSubmit={handleSubmit} className="space-y-5">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Votre Nom *"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Adresse E-mail *"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
-                />
-              </div>
-
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  name="subject"
-                  placeholder="Sujets *"
-                  value={formData.subject}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
-                />
-                <input
-                  type="tel"
-                  name="phone"
-                  placeholder="Mobile *"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
-                />
-              </div>
-
-              <textarea
-                name="message"
-                placeholder="Message *"
-                rows={6}
-                value={formData.message}
+        <motion.div
+          initial={{ y: 50, opacity: 0, scale: 0.95 }}
+          whileInView={{ y: 0, opacity: 1, scale: 1 }}
+          viewport={{ once: false, amount: 0.5 }}
+          transition={{
+            duration: 1.5,
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+          }}
+          className="lg:col-span-8 px-0 md:pl-16 pb-8"
+        >
+          <form onSubmit={handleSubmit} className="space-y-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <input
+                type="text"
+                name="name"
+                placeholder="Votre Nom *"
+                value={formData.name}
                 onChange={handleChange}
                 required
-                className="w-full p-6 pb-20 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all resize-none"
+                className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
               />
+              <input
+                type="email"
+                name="email"
+                placeholder="Adresse E-mail *"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
+              />
+            </div>
 
-              <button
-                type="submit"
-                disabled={loading}
-                className="flex items-center gap-2 mt-4 text-white md:text-[16px] text-sm py-4 px-14 rounded-full bg-[#FF5722] hover:bg-black transition-all duration-300 transform hover:scale-105 shadow-lg shadow-orange-200 hover:shadow-black/50 disabled:opacity-70 disabled:cursor-not-allowed"
-              >
-                {loading ? (
-                  <>
-                    <Spinner />
-                    Envoi...
-                  </>
-                ) : (
-                  <>
-                    Envoyer
-                    <ArrowUpRight size={20} />
-                  </>
-                )}
-              </button>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <input
+                type="text"
+                name="subject"
+                placeholder="Sujets *"
+                value={formData.subject}
+                onChange={handleChange}
+                required
+                className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
+              />
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Mobile *"
+                value={formData.phone}
+                onChange={handleChange}
+                className="w-full p-5 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all"
+              />
+            </div>
 
-              {responseMessage && (
-                <p className="text-center text-sm mt-3">{responseMessage}</p>
+            <textarea
+              name="message"
+              placeholder="Message *"
+              rows={6}
+              value={formData.message}
+              onChange={handleChange}
+              required
+              className="w-full p-6 pb-20 bg-[#F6F9FC] border-none rounded-xl focus:ring-2 focus:ring-gray-400 outline-none transition-all resize-none"
+            />
+
+            <button
+              type="submit"
+              disabled={loading}
+              className="flex items-center gap-2 mt-4 text-white md:text-[16px] text-sm py-4 px-14 rounded-full bg-[#FF5722] hover:bg-black transition-all duration-300 transform hover:scale-105 shadow-lg shadow-orange-200 hover:shadow-black/50 disabled:opacity-70 disabled:cursor-not-allowed"
+            >
+              {loading ? (
+                <>
+                  <Spinner />
+                  Envoi...
+                </>
+              ) : (
+                <>
+                  Envoyer
+                  <ArrowUpRight size={20} />
+                </>
               )}
-            </form>
-          </motion.div>
-        </div>
+            </button>
+
+            {responseMessage && (
+              <p className="text-center text-sm mt-3">{responseMessage}</p>
+            )}
+          </form>
+        </motion.div>
       </div>
 
       {/* Decorative Image */}
